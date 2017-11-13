84548e839e92ef2614d954527918f0f266cf0fee
medude <varner_nathan@yahoo.com>
2017-11-13 18:31:59 -0500

    Still doesn't do anything

--- health_loaders/Fitbit.py
+++ health_loaders/Fitbit.py
@@ -1,5 +1,5 @@
 import fitbit
 
 class Fitbit:
-    def __init__:
-        self.auth_client = fitbit.Fitbit
+    def __init__(self):
+        self.auth_client = fitbit.Fitbit("22CLGZ", "3e1950cee150a49842cc9ee22c5af68c")
